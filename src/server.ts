ac05f7b018b2ba6798fc52101a199456f771fa5f
Michael Legg <michael.legg@outlook.com>
2020-11-25 23:01:04 +0000

    - Installed socket.io & tslib - Setup basic server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
 
+import express from "express";
+import { Server as SocketIoServer, Socket} from "socket.io";
+
+const port = process.env.port || 3001
+const app: express.Application = express();
+
+const server = app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`)
+});
+const io = new SocketIoServer(server, { transports: ["websocket"] });
+
+io.on('connect', (socket: Socket) => {
+    console.log(`connected ${socket.id}`)
+});
+
+
